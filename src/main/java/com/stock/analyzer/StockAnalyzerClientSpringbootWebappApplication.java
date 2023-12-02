d8ca37905c9f759e5719256abb33b618e4c914a8
ravaneswaran <ravaneswaran@gmail.com>
2023-12-02 16:00:08 +0530

    initial commit

---
+++ src/main/java/com/stock/analyzer/StockAnalyzerClientSpringbootWebappApplication.java
@@ -0,0 +1,13 @@
+package com.stock.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyzerClientSpringbootWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalyzerClientSpringbootWebappApplication.class, args);
+	}
+
+}
