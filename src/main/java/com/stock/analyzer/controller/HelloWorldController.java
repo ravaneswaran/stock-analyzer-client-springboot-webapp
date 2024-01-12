3b60b6dd5e36d34c95609cc9c44cc0da980c4b00
ravaneswaran <ravaneswaran@gmail.com>
2024-01-12 13:36:44 +0530

    hello world controller added

--- src/main/java/com/stock/analyzer/controller/HelloWorldController.java
+++ src/main/java/com/stock/analyzer/controller/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.stock.analyzer.controller;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String index(){
+        return "Hello World !!!";
+    }
 }
